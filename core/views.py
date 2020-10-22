106a37e6b98f1ca753feb4eb6eed2c4a030cf296
Dnevb <villalbautria08@gmail.com>
2020-10-22 14:42:14 -0500

    Core app

--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+class HomeView(View):
+    def get(self, request):
+        return render(request, "home.html")
+
+    def post(self, request):
+        context = {
+            'resultado': request.POST['nit']
+        }
+        return render(request, 'resultado.html', context)
